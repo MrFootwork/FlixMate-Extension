d50a7f660681b6167848e233263fc93e4fc2875e
Porcari-Karim <porcari.karim1@gmail.com>
2024-12-16 13:18:07 +0100

    Added bundle with vite, the app now renders on a new div inside netflix instead of the #root, added sockets and room joinDD

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,91 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { io } from 'socket.io-client'
+// import './App.css'
+
+const WS_URL = import.meta.env.VITE_WS_URL
+console.log(WS_URL)
+
+let params = null
+let room = null
+
+if (window.location.search) {
+  const getQuery = window.location.search.split('?')[1]
+  params = getQuery.split('&')
+  params.forEach(param => {
+    const [key, value] = param.split('=')
+    if (key == 'flixmate') room = value
+  })
+}
+
+// console.log(window.location)
 
 function App() {
   const [connected, setConnected] = useState(null)
   const [user, setUser] = useState(null)
+  const [socket, setSocket] = useState(null)
+  // console.log(params, room)
 
+  // Checks if the user is still connected
   useEffect(() => {
     chrome.storage.sync.get(['token'], token => {
       if (token) {
         setConnected(token.token)
         chrome.storage.sync.get(['user'], result => {
           setUser(result.user)
-          console.log(result.user)
+          // console.log(result.user)
         })
       }
     })
     setTimeout(() => setConnected(null), 2000)
   }, [connected])
 
-  return <>{user ? user.email : 'Please connect'}</>
+  // connects the socket one time when the user is retreived
+  useEffect(() => {
+    if (connected && !socket) {
+      setSocket(
+        io(WS_URL, {
+          auth: {
+            token: connected,
+          },
+        })
+      )
+    }
+  }, [connected])
+
+  // Runs on socket connection to set-up the events
+  useEffect(() => {
+    if (socket) {
+      console.log('Trying to join room ', room)
+      socket.emit('join-room', room)
+      socket.on('joined-room', () => {
+        console.log(`Succesfully joined the room ${room}`)
+      })
+    }
+  }, [socket])
+
+  return (
+    <div className='flixmateApp'>
+      <style>
+        {`.flixmateApp{
+        position: absolute;
+        width: 500px;
+        aspect-ratio: 1;
+        z-index: 50000;
+        top: 0;
+        left: 0;
+        background-color: white;
+        color: black;
+        font-size: larger;
+        }`}
+      </style>
+      {connected ? (
+        <></>
+      ) : (
+        //all the wotk should be done inside here, no components outside of this
+        <>{user ? user.email : 'Please connect'}</>
+      )}
+    </div>
+  )
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,27 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+function loadCSS(url, callback) {
+  const head = document.getElementsByTagName('head')[0]
+  const cssnode = document.createElement('link')
+
+  cssnode.type = 'text/css'
+  cssnode.rel = 'stylesheet'
+  cssnode.href = url
+
+  cssnode.onreadystatechange = callback
+  cssnode.onload = callback
+
+  head.appendChild(cssnode)
+}
+
+// loadCSS('flixmate.css', null)
+
+const flixMateRoot = document.createElement('div')
+console.log(flixMateRoot, document.querySelector('body'))
+document.querySelector('body').appendChild(flixMateRoot)
+createRoot(flixMateRoot).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
